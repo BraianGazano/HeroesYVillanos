c6e20bf10b07cf3b916cb46269fd550ce28b5126
BraianGazano <gazanobraian@gmail.com>
2022-01-17 19:12:05 -0300

    Comparators agregados

--- src/comparators/ComparatorCompuesto.java
+++ src/comparators/ComparatorCompuesto.java
@@ -1,14 +1,46 @@
 package comparators;
 
 import java.util.Comparator;
+import java.util.LinkedList;
+import java.util.List;
 
 import juego.Enfrentable;
 
 public class ComparatorCompuesto implements Comparator<Enfrentable> {
 
+    private List<Comparator<Enfrentable>> comparators;
+
+    public ComparatorCompuesto() {
+        this.comparators = new LinkedList<Comparator<Enfrentable>>();
+    }
+
+    public ComparatorCompuesto(List<Comparator<Enfrentable>> comparators) {
+        this.comparators = comparators;
+    }
+
+    public boolean addComparator(Comparator<Enfrentable> c) {
+        if (c == this) {
+            return false;
+        }
+        return comparators.add(c);
+    }
+
+    public boolean addComparator(Comparator<Enfrentable> c, int index) {
+        if (c == this) {
+            return false;
+        }
+        comparators.add(index, c);
+        return true;
+    }
+
     @Override
-    public int compare(Enfrentable arg0, Enfrentable arg1) {
-        // TODO Auto-generated method stub
+    public int compare(Enfrentable e1, Enfrentable e2) {
+        int result;
+        for (Comparator<Enfrentable> c : comparators) {
+            result = c.compare(e1, e2);
+            if (result != 0)
+                return result;
+        }
         return 0;
     }
 
--- src/comparators/ComparatorCriterioSimple.java
+++ src/comparators/ComparatorCriterioSimple.java
@@ -6,10 +6,18 @@ import juego.Enfrentable;
 
 public class ComparatorCriterioSimple implements Comparator<Enfrentable> {
 
+    private String atributo;
+
+    public ComparatorCriterioSimple(String atributo) {
+        this.atributo = atributo;
+    }
+
     @Override
-    public int compare(Enfrentable arg0, Enfrentable arg1) {
-        // TODO Auto-generated method stub
-        return 0;
+    public int compare(Enfrentable e1, Enfrentable e2) {
+
+        Float valor1 = e1.getValorAtributo(atributo);
+        Float valor2 = e2.getValorAtributo(atributo);
+        return valor1.compareTo(valor2);
     }
 
 }
