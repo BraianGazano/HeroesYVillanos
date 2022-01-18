8760a4b291b9bd703c56536d205f722c4fcdba3c
BraianGazano <gazanobraian@gmail.com>
2022-01-18 18:05:50 -0300

    Metodo add de Liga agregado

--- src/juego/Liga.java
+++ src/juego/Liga.java
@@ -1,12 +1,27 @@
 package juego;
 
+import java.util.LinkedList;
 import java.util.List;
 
 public class Liga extends Enfrentable {
 
+    private List<Enfrentable> integrantes;
+
     public Liga(String nombre, String nombreFantasia) {
         super(nombre, nombreFantasia);
+        this.integrantes = new LinkedList<Enfrentable>();
+    }
 
+    public Liga(String nombre, String nombreFantasia, List<Enfrentable> integrantes) {
+        super(nombre, nombreFantasia);
+        this.integrantes = integrantes;
+    }
+
+    public boolean addIntegrante(Enfrentable e) {
+        if (e != this) {
+            return integrantes.add(e);
+        }
+        return false;
     }
 
     @Override
