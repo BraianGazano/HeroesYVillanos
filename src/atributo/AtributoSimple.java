13134fdff4454834d511127dd6a403fb1324e81a
Braian <44708422+BraianGazano@users.noreply.github.com>
2022-01-19 18:26:24 -0300

    SetValor agregado

--- src/atributo/AtributoSimple.java
+++ src/atributo/AtributoSimple.java
@@ -7,12 +7,15 @@ public class AtributoSimple implements Atributo {
     private float value;
 
     public AtributoSimple(float value) {
-        this.value = value;
+        setValor(value);
     }
 
     @Override
     public float getValor(Enfrentable e) {
         return value;
     }
-
+    
+    public void setValor(float value){
+    	this.value = value;
+    }
 }
