c9714b473b2f87c4739cb45b13147a2671cce6e3
BraianGazano <gazanobraian@gmail.com>
2022-01-19 16:55:06 -0300

    Estructura del Juego armada

--- src/juego/Juego.java
+++ src/juego/Juego.java
@@ -1,5 +1,30 @@
 package juego;
 
+import java.util.Comparator;
+import java.util.LinkedList;
+import java.util.List;
+
 public class Juego {
 
+    private List<Enfrentable> enfrentables;
+
+    public Juego() {
+        this.enfrentables = new LinkedList<Enfrentable>();
+    }
+
+    public List<Enfrentable> getQuienesVencen(Enfrentable e, Comparator<Enfrentable> c) {
+        return null;
+    }
+
+    public Enfrentable enfrentar(Enfrentable e1, Enfrentable e2, Comparator<Enfrentable> c) {
+        return null;
+    }
+
+    public boolean addEnfrentable(Enfrentable e) {
+        return false;
+    }
+
+    public List<Personaje> ordenarPersonajes(Comparator<Enfrentable> c) {
+        return null;
+    }
 }
